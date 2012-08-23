28873e049bfd537f1df75899992f46c7bf47e109
tkn <tkn@zuehlke.com>
2012-08-23 11:59:46 +0200

    fix foodcritic #007 complaint by adding cookbook name to metadata.rb (otherwise you will get a warning if you have checked out to a different directory

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs subversion"
 version           "1.0.0"
+name              "subversion"
 
 %w{ redhat centos fedora ubuntu debian }.each do |os|
   supports os
